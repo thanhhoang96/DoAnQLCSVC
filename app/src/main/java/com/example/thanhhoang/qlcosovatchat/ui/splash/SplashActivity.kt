89ce5dea380bcaac295c3908cbb9a8c1e458c61c
thanhhoang96 <hoangngocthanhcntt96@gmail.com>
2018-11-06 00:34:16 +0700

    change file splash

--- app/src/main/java/com/example/thanhhoang/qlcosovatchat/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/example/thanhhoang/qlcosovatchat/ui/splash/SplashActivity.kt
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity
 import com.example.thanhhoang.qlcosovatchat.MainActivity
 import com.example.thanhhoang.qlcosovatchat.R
 import com.example.thanhhoang.qlcosovatchat.data.source.repository.Repository
+import com.example.thanhhoang.qlcosovatchat.ui.login.LoginActivity
 
 class SplashActivity : AppCompatActivity() {
 
@@ -18,8 +19,7 @@ class SplashActivity : AppCompatActivity() {
         viewModel = SplashViewModel(Repository())
 
         Handler().postDelayed({
-            //            startActivity(Intent(this, if (viewModel.hasAccessToken()) MainActivity::class.java else LoginActivity::class.java))
-            startActivity(Intent(this,MainActivity::class.java))
+            startActivity(Intent(this, if (viewModel.hasAccessToken()) MainActivity::class.java else LoginActivity::class.java))
             finish()
         }, 2000)
     }
